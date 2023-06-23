aac4b01f16c081b6b56f510e12edf1f0e998e87e
Diego Andress Salas <diegoandress717>
2023-06-23 18:31:29 -0500

    se agregó skeleton al navbar

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSession } from "next-auth/react";
-import { redirectIfUnauthenticated, renderLoadingModal } from "../../helper.js";
+import { redirectIfUnauthenticated, renderLoadingModal } from "../../utils/index.js";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
